baa34d06c7b065bdad027fd81b5c0352cbaef572
aldik <aldi001122@gmail.com>
2022-04-15 11:44:04 +0600

    Improved routes

--- server.js
+++ server.js
@@ -2,25 +2,21 @@ const express = require('express')
 const app = express();
 const PORT = 3000;
 
-// app.route('/').get((req,res) => {
-//     res.sendFile(__dirname+'/index.html')
-// })
-// app.route('/home').get((req,res) => {
-//     res.sendFile(__dirname+'/home.html')
-// })
-// app.route('/restaurants').get((req,res) => {
-//     res.sendFile(__dirname+'/restaurants.html')
-// })
+app.use('/', require('./routes/indexRouter'))
+app.use('/home', require('./routes/homeRouter'))
+app.use('/restaurants', require('./routes/restaurantsRouter'))
+
+
 // ______________________________________________
-app.get('/', ((req,res) => {
-    res.sendFile(__dirname+'/index.html')
-}))
-app.get('/home', ((req,res) => {
-    res.sendFile(__dirname+'/home.html')
-}))
-app.get('/restaurants', ((req,res) => {
-    res.sendFile(__dirname+'/restaurants.html')
-}))
+// app.get('/', ((req,res) => {
+//     res.sendFile(__dirname+'/index.html')
+// }))
+// app.get('/home', ((req,res) => {
+//     res.sendFile(__dirname+'/home.html')
+// }))
+// app.get('/restaurants', ((req,res) => {
+//     res.sendFile(__dirname+'/restaurants.html')
+// }))
 app.listen(PORT, () =>
     console.log(`App listening at localhost: ${PORT}`)
 );
