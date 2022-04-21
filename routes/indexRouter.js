c6e2ac436d9517acfab74f3b7c494d9becb79304
aldik <aldi001122@gmail.com>
2022-04-21 15:04:34 +0600

    Currency api

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const http = require("http");
+
+let currency
+
+const KEY = '7660472a37590471718968e51dc9710d'
+let url = 'http://api.exchangeratesapi.io/v1/latest?access_key='+ KEY +'&symbols=USD,KZT'
+http.get(url, function (response){
+    response.on('data', data => {
+        currency = JSON.parse(data)
+        console.log(currency)
+    })
+})
 
 router
     .route('/')
     .get((req,res) => res.render(path.resolve('views/index.ejs'),
-        {title: 'Menu', activePage: 'index'}))
+        {title: 'Menu', activePage: 'index',priceUSD:currency.rates.USD, priceKZT:currency.rates.KZT}))
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use('/home', require('./routes/homeRouter'))
 app.use('/restaurants', require('./routes/restaurantsRouter'))
 app.use('/sign', require('./routes/signRouter'))
 
-
 // const KEY = '7660472a37590471718968e51dc9710d'
 // let url = 'http://api.exchangeratesapi.io/v1/latest?access_key='+ KEY +'&symbols=USD,KZT'
 // http.get(url, function (response){
@@ -20,9 +19,9 @@ app.use('/sign', require('./routes/signRouter'))
 //         let currency = JSON.parse(data)
 //         let usd = currency.rates.USD
 //         let kzt = currency.rates.KZT
+//         console.log(currency)
 //     })
 // })
-
 
 app.listen(PORT, () =>
     console.log(`App listening at localhost: ${PORT}`)
