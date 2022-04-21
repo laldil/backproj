587ccaf4933969fae13e1896701d158fc210aa6c
aldik <aldi001122@gmail.com>
2022-04-21 16:00:32 +0600

    Currency api

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
-const http = require("http");
+const http = require('http');
+
 
 let currency
-
 const KEY = '7660472a37590471718968e51dc9710d'
 let url = 'http://api.exchangeratesapi.io/v1/latest?access_key='+ KEY +'&symbols=USD,KZT'
 http.get(url, function (response){
     response.on('data', data => {
         currency = JSON.parse(data)
-        console.log(currency)
     })
 })
 
